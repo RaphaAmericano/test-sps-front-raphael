6926da4b8cbd2258ba6994ee36de7afc86417709
Raphael Americano <raphael@raphaelamericano.com.br>
2024-04-14 13:51:12 -0300

    fix navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,20 @@
-import { LayoutDashboardIcon, SettingsIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
 
 function Navbar(){
     return (
         <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white font-bold text-xl">SPS Group</div>
+          <div className="text-white font-bold text-xl"><Link href="/home">SPS Group</Link></div>
           <div className="flex space-x-4">
-            <Link href="#">
-              <Link className="text-white" href="#">
-                <LayoutDashboardIcon className="h-4 w-4" />
-                <span className="sr-only">Dashboard</span>
+              <Link className="text-white" href="/users">
+                Usuários
               </Link>
-            </Link>
-            <Link href="#">
-              <Link className="text-white" href="#">
-                <UsersIcon className="h-4 w-4" />
-                <span className="sr-only">Users</span>
+              <Link className="text-white" href="/">
+                Sair                
               </Link>
-            </Link>
-            <Link href="#">
-              <Link className="text-white" href="#">
-                <SettingsIcon className="h-4 w-4" />
-                <span className="sr-only">Settings</span>
-              </Link>
-            </Link>
           </div>
         </div>
       </nav>
     )
 }
-
 export default Navbar
